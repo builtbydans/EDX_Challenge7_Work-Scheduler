9127a442fa20a0ff9bdae1780d2598447595780b
Danish Shafi <danish.shafi@live.co.uk>
2023-01-24 20:47:25 +0000

    Setup main structure for timeblocks

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-var currentDay = $('#currentDay');
+// DOM elements
+var currentDay = $("#currentDay");
+var hourBlock = $("#hour");
+var timeBlock = $("#timeBlock");
+var saveBlock = $("#save");
+
+// Moment.js variables
 var now = moment().format("dddd, MMMM Do YYYY");
 
+// Global Variables
+
+// --------------------------------------------------->
+// START OF PROGRAM
+// --------------------------------------------------->
+
+// Setting Current Day to top of the calendar
 $(currentDay.text(now));
+
+// Adding Rows dynamically using the DOM
+for (var i = 0; i < 8; i++) {
+  var hourCol = $("<div>").addClass("row hour").text("hour");
+  var blockCol = $("<div>").addClass("row").text("timeblock");
+  var saveCol = $("<div>").addClass("row saveBtn").text("save")
+
+  $(hour).append(hourCol);
+  $(timeBlock).append(blockCol);
+  $(saveBlock).append(saveCol);
+}
